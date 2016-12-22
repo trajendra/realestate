36df8c1e592e864e867736beb75aef8845bf9357
Rajendra <mail2raajj@gmail.com>
2016-12-22 14:13:31 +0000

    heroku dependencies added

--- raj_django_realestate/settings.py
+++ raj_django_realestate/settings.py
@@ -154,6 +154,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+# heroku storage
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
